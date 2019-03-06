0137e52024fe4dcfc4e28beb6622f6f277f17851
symonk <jackofspaces@gmail.com>
2019-03-06 18:15:48 +0000

    tidy up, importing and find(s)

--- pylenium/core/pylenium.py
+++ pylenium/core/pylenium.py
@@ -3,15 +3,22 @@ import threading
 from pylenium.config.config import PyleniumConfig
 from pylenium.drivers.driver_manager import PyleniumDriver
 from pylenium.drivers.driver_strategy import *
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote import webelement
 
 config = PyleniumConfig()
 driver = threading.local()
 driver.x = None
 
 
+# Test entry point! just simply... get started, provide us a URL and we will get you a thread local driver if necessary!
 def start(url: str) -> PyleniumDriver:
     if isinstance(driver.x, PyleniumDriver):
         return driver.x.goto(url)
     print('Hey, you have a thread local driver duh!')
     driver.x = PyleniumDriver(PyleniumConfig(), ChromeBrowserStrategy())
     return driver.x.goto(url)
+
+
+def find(by: By) -> webelement:
+    return driver.x.find_element(by)
