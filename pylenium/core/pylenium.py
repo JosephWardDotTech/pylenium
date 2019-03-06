7030c46fd23c0215ba8ffa4fb36a17ed58cb13ec
symonk <jackofspaces@gmail.com>
2019-03-06 17:58:21 +0000

    fix imports

--- pylenium/core/pylenium.py
+++ pylenium/core/pylenium.py
@@ -1,4 +1,5 @@
 from pylenium.config.config import PyleniumConfig
+from pylenium.drivers.driver_manager import PyleniumDriver
 from pylenium.drivers.driver_strategy import *
 
 config = PyleniumConfig()
