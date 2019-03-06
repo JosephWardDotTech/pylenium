9a2de2ff8d9ee735b7970576acf70498fdfad536
symonk <jackofspaces@gmail.com>
2019-03-06 17:57:24 +0000

    config / driver shuffling

--- pylenium/drivers/driver_manager.py
+++ pylenium/drivers/driver_manager.py
@@ -1,4 +1,33 @@
+from __future__ import annotations
+from pylenium.config.config import PyleniumConfig
+from pylenium.drivers.driver_strategy import *
 
 
 class PyleniumDriver(object):
-    pass
+    def __init__(self,
+                 config: PyleniumConfig,
+                 driver_strategy: AbstractBrowserStrategy = ChromeBrowserStrategy()):
+        self._config = config
+        self.driver_strategy = driver_strategy
+        self._driver: webdriver = self.driver_strategy.instantiate()
+
+    @property
+    def driver(self) -> webdriver:
+        return self._driver
+
+    @driver.setter
+    def driver(self, value):
+        self._driver = value
+        if self._config.browser_maximized:
+            self.maximize()
+
+    def goto(self, url: str) -> PyleniumDriver:
+        self.driver.get(url)
+        return self
+
+    def maximize(self) -> PyleniumDriver:
+        self.driver.maximize_window()
+        return self
+
+    def quit(self):
+        self.driver.quit()
--- pylenium/drivers/driver_strategy.py
+++ pylenium/drivers/driver_strategy.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 from abc import abstractmethod, ABC
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
-from pylenium.config.config import PyleniumConfig
 from selenium import webdriver
 
 
@@ -30,32 +29,3 @@ class RemoteWebDriverBrowserStrategy(AbstractBrowserStrategy):
 
     def instantiate(self):
         pass
-
-
-class PyleniumDriver(object):
-    def __init__(self,
-                 config: PyleniumConfig,
-                 driver_strategy: AbstractBrowserStrategy = ChromeBrowserStrategy()):
-        self.driver_strategy = driver_strategy
-        self._driver: webdriver = self.driver_strategy.instantiate()
-
-    @property
-    def driver(self) -> webdriver:
-        return self._driver
-
-    @driver.setter
-    def driver(self, value):
-        self._driver = value
-        if self._config.browser_maximized:
-            self.maximize()
-
-    def goto(self, url: str) -> PyleniumDriver:
-        self.driver.get(url)
-        return self
-
-    def maximize(self) -> PyleniumDriver:
-        self.driver.maximize_window()
-        return self
-
-    def quit(self):
-        self.driver.quit()
