a7dcd17df095d703721cca28cb95d4c251e3d0d1
symonk <jackofspaces@gmail.com>
2019-04-21 20:20:08 +0100

    [master]:rocket: add should have attribute

--- conditions/condition.py
+++ conditions/condition.py
@@ -31,5 +31,10 @@ class attribute(PyCondition):  # NOSONAR
     def __init__(self, expected: str):
         self.expected = expected
 
-    def evaluate(self, py_element):
-        pass
+    def evaluate(self, proxy) -> bool:
+        try:
+            assert proxy.wrapped_element.get_attribute(self.expected) is not None
+        except AssertionError:
+            raise UIAssertionException('Expected element have the attribute: {} but it did not'.format(
+                self.expected
+            ))
