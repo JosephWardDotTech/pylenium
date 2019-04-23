c8c9557fea413222ed2f64e6a3523d6b1c2b33f6
symonk <jackofspaces@gmail.com>
2019-04-23 18:17:15 +0100

    [master]:rocket: http server implemented

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,4 +1,7 @@
 import logging
+import subprocess
+import sys
+import time
 
 import pytest
 
@@ -8,9 +11,17 @@ from core.pylenium import terminate, start
 log = logging.getLogger("pylenium")
 
 
+@pytest.fixture(scope='session', autouse=True)
+def web_server_for_integration_tests():
+    import http.server
+    subprocess.Popen('python -m http.server -d ./tests/server/static_content')
+    time.sleep(5)
+    yield
+
+
 @pytest.fixture(scope="function", autouse=True)
 def manage_test(request):
-    PyleniumConfig().base_url = "http://localhost:8000/tests/server/static_content/"
+    PyleniumConfig().base_url = "http://localhost:8000/"
     start(request.node.get_closest_marker("IT").kwargs["page"])
     yield
     terminate()
